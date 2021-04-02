d16ee7bba612d2bc90e13ece327da532115cb718
Enis Inan <enis.inan@puppet.com>
2021-04-02 12:25:08 -0700

    (PE-31683) Bump to Puppet 7 for pe-bolt-server main
    Signed-off-by: Enis Inan <enis.inan@puppet.com>

--- configs/projects/pe-bolt-server-runtime-main.rb
+++ configs/projects/pe-bolt-server-runtime-main.rb
@@ -4,5 +4,8 @@ project 'pe-bolt-server-runtime-main' do |proj|
   # for gem installs instead of --no-ri --no-rdoc. This setting allows us to use this while we support both ruby 2.5 and 2.7
   # Once we are no longer using ruby 2.5 we can update.
   proj.setting(:no_doc, true)
+  proj.setting(:rubygem_puppet_version, '7.4.1')
   instance_eval File.read(File.join(File.dirname(__FILE__), '_shared-pe-bolt-server.rb'))
+  # Include Puppet 7 dependency
+  proj.component('rubygem-scanf')
 end
